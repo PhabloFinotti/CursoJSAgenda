ef2dab1c55dbf2c5f29b35516e774d4e6b7b5481
Phablo Finotti <phablofinotti@gmail.com>
2023-01-09 07:41:14 -0300

    feat: Cadastro, login, cadastro de contatos

--- src/middlewares/middleware.js
+++ src/middlewares/middleware.js
@@ -1,6 +1,7 @@
 exports.middlewareGlobal = (req, res, next) => {
   res.locals.errors = req.flash('errors');
   res.locals.success = req.flash('success');
+  res.locals.user = req.session.user ? req.session.user : null;
   next();
 };
 
@@ -15,5 +16,15 @@ exports.checkCsrfError = (err, req, res, next) => {
 
 exports.csrfMiddleware = (req, res, next) => {
   res.locals.csrfToken = req.csrfToken();
+  next();
+};
+
+exports.loginRequired = (req, res, next) => {
+  if (!req.session.user) {
+    req.flash('errors', 'Você precisa fazer login.');
+    req.session.save(() => res.redirect('/'));
+    return;
+  }
+
   next();
 };
